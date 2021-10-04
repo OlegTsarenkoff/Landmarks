0b44c265f0bb110c15c1d4a53a601e011fb02115
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-10-04 14:43:30 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Oleg Tsarenkoff on 4.10.21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
